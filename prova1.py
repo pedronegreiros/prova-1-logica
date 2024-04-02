3ac5bab2848da5f7a606523f97bb2c8728d50a1f
Pedro Negreiros <146891764+pedronegreiros@users.noreply.github.com>
2024-04-02 15:59:25 -0300

    Add files via upload

---
+++ prova1.py
@@ -0,0 +1,34 @@
+#PROVA 1 LÓGICA PROGRAMAÇÃO ALUNO: PEDRO NEGREIROS
+# Exemplos de tipos de variáveis diferentes
+
+int = 42
+
+ponto_flutuante = 3.14
+
+texto = "Olá, Mundo!"
+
+booleano = True
+
+
+
+# Solicitação de dados ao usuário
+
+nome = input("Por favor, digite seu nome: ")
+
+
+
+# Imprime a solicitação anterior na tela com mensagem personalizada
+
+print(f"Olá, {nome}!")
+
+
+
+# Exemplo uso das variáveis
+
+print(f"Um número inteiro: {int}")
+
+print(f"Um número de ponto flutuante: {ponto_flutuante}")
+
+print(f"Uma string: {texto}")
+
+print(f"Uma variável booleana: {booleano}")
